09d72270b7495a7957fde7218a07c76f3151e84f
Jesús Rubén Esparza Soto <rubenesparzasoto95@gmail.com>
2022-01-18 23:39:09 -0700

    (learn-with-project): CoreSchool

---
+++ CoreSchool/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CoreSchool
+{
+	class School
+	{
+		public string name;
+		public string address;
+		public int foundationYear;
+		public string CEO;
+
+		public void Ring() {
+			Console.Beep(1000, 1000);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args) {
+			School mySchool = new School();
+			
+			mySchool.name = "Platzi Academy";
+			mySchool.address = "Cr 9 Calle 72";
+			mySchool.foundationYear = 2012;
+
+			Console.WriteLine("Timbre");
+
+			mySchool.Ring();
+		}
+	}
+}
